8d33cfe4f4ab2c35585b389d23e008c48fbcef14
dain <57316771+DainPark-web@users.noreply.github.com>
2023-02-07 15:06:46 +0100

    update

---
+++ js/animation/index.js
@@ -0,0 +1,6 @@
+
+
+gsap.from(".an-home-title1", {y: 200, duration: 2, delay: 0, opacity: 0, ease: Power3.
+easeOut})
+gsap.from(".an-home-title2", {y: 200, duration: 2, delay: 0.2, opacity:0, ease: Power3.
+easeOut})
